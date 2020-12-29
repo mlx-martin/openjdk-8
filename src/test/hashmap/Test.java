d1bdca5ba09cd8935c4a1e56ceb5dfa1fba6987c
martin <chinahtkh1998@163.com>
2020-12-29 15:26:09 +0800

    Initial commit

---
+++ src/test/hashmap/Test.java
@@ -0,0 +1,12 @@
+package test.hashmap;
+
+import java.util.HashMap;
+
+public class Test {
+
+    public static void main(String[] args) {
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("K1",0.1);
+        System.out.println("hello world");
+    }
+}
